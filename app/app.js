3276e2ffc5baf0a81a77aa2e032b2e7c85cf8351
Arun Khanna <arunkhanna00@gmail.com>
2016-11-21 21:58:49 -0800

    edit post updates for testing

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ angular.module("myApp", [])
   // Create an JSON array of all posts(to be changed later)
   $scope.posts = [
     {title: 'post 1', upvotes: 6},
-    {title: 'post 2', upvotes: 34},
+    {title: 'post 2', upvotes: 134},
     {title: 'post 3', upvotes: 21},
     {title: 'post 4', upvotes: 43},
     {title: 'post 5', upvotes: 12},
