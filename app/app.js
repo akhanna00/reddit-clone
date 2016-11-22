0400e4ced7e3856f9c99e0a6a46fefcea87a0dd8
Arun Khanna <arunkhanna00@gmail.com>
2016-11-21 20:47:35 -0800

    create initial json

--- app/app.js
+++ app/app.js
@@ -1,5 +1,14 @@
+// Create module
 angular.module("myApp", [])
 
+// Create main controller
 .controller("mainController", ['$scope', function($scope) {
-  $scope.hello = "hello world";
+  // Create an JSON array of all posts(to be changed later)
+  $scope.posts = [
+    {title: 'post 1', upvotes: 6},
+    {title: 'post 2', upvotes: 34},
+    {title: 'post 3', upvotes: 21},
+    {title: 'post 4', upvotes: 43},
+    {title: 'post 5', upvotes: 12},
+  ];
 }]);
