728bd7344cf43d78570366b16dbdaa17f05588d9
Arun Khanna <arunkhanna00@gmail.com>
2016-11-21 21:07:05 -0800

    create a function that allows posts to be added

--- app/app.js
+++ app/app.js
@@ -13,8 +13,15 @@ angular.module("myApp", [])
   ];
   // Create a function to add posts
   $scope.addPost = function() {
-    if (!$sope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: 'A new post', upvotes: 0});
+    // Make sure title is not empty
+    if (!$scope.title || $scope.title === '') { return; }
+    // Add post with title and link from view
+    $scope.posts.push(
+      {title: $scope.title, link: $scope.link, upvotes: 0});
+    // Clear out title and link
+    $scope.title = '';
+    $scope.link = '';
+
   }
 
   // Create a function to add upvotes
